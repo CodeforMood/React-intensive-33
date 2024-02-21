917f93d7fe39645b198c8c51b1ac9a2d84b25bf0
Vladislav <98046355+CodeforMood@users.noreply.github.com>
2024-02-21 11:28:11 +0000

    fix button behavior and add changeHandler

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,5 @@
 import './App-style.css';
-import React, { Component } from 'react';
+import React, { ChangeEvent, Component, MouseEvent } from 'react';
 
 type State = {
   count: number;
@@ -9,20 +9,29 @@ export default class App extends Component {
   constructor(props: any) {
     super(props);
     this._handleClick = this._handleClick.bind(this);
+    this._handleChange = this._handleChange.bind(this);
   }
-  _handleClick() {
-    this.setState((prevState: State) => ({count: ++prevState.count}));
+  _handleClick(evt: MouseEvent<HTMLButtonElement>) {
+    evt.preventDefault()
+    this.setState(() => ({count: +this.state.count + 1}));
   }
   
   readonly state: State = {
     count: 0,
   };
   
+  _handleChange(evt: ChangeEvent<HTMLInputElement>): void {
+    this.setState(() => ({
+        count: evt.target.value,
+      })
+    )
+  }
+
   render() {
     return (
       <form className="form">
-        <input className="form" value={this.state.count} />
-        <button className="form" onClick={this._handleClick}>Increment</button>
+        <input className="form__input" onChange={this._handleChange} value={this.state.count}/>
+        <button className="form__button" onClick={this._handleClick}>Increment</button>
       </form>
     );
   }
